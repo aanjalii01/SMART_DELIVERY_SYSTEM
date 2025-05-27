4d1b163f6261e67aa5c5af5bf2b3c4e2e8ae2d5c
Anjali Sinha <144527756+aanjalii01@users.noreply.github.com>
2025-05-27 18:34:36 +0530

    Delete smart_delivery_system directory

--- smart_delivery_system/simulation/batch_optimizer.py
+++
@@ -1,3 +0,0 @@
-def sort_by_priority(orders):
-    priority_map = {'emergency': 1, 'medical': 2, 'normal': 3}
-    return sorted(orders, key=lambda o: priority_map[o['type']])
--- smart_delivery_system/simulation/order_manager.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def simulate_delivery(order, route, delay_callback, finish_callback):
-    total_steps = len(route)
-    for i, point in enumerate(route):
-        time.sleep(2)  # simulate time delay
-        delay_callback(i + 1, total_steps, point)
-    finish_callback()
